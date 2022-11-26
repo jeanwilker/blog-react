5d8d10f385ae9078b709f7bff5973065adf160e1
Jean Wilker <jeanwilkerss@gmail.com>
2022-11-25 21:55:49 -0300

    Cria interação com o json server.

--- src/pages/Home/Main/index.jsx
+++ src/pages/Home/Main/index.jsx
@@ -1,27 +1,45 @@
-const Main = () => {
+// Hooks
+import { useState, useEffect } from 'react';
+
+// API
+import api from '../../../services/api';
+
+const Main = ({ content }) => {
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        if (content) {
+            api.get('/user/' + content.id_user).then((response) => {
+                setUser(response.data);
+            });
+        }
+    }, []);
+
     return (
         <>
             <div className="grid-6">
                 <div className="bb-black py-3">
-                    <h6 className="color-gray">01 NOV 2022</h6>
-                    <h6 className="upercase color-primary">Tecnologia</h6>
-
-                    <h4>O que tem de novo no PS5? Muitas novidades? </h4>
-                    <p className="mt-1">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Ornare urna pharetra ut ac, pellentesque.
-                    </p>
-                    <div className="flex-start-row mt-3">
+                    <h6 className="color-gray">{content.date}</h6>
+                    <h6 className="upercase color-primary">
+                        {content.category}
+                    </h6>
+                    <a href="" className="link-title">
+                        <h4 className="mt-1">{content.title}</h4>
+                    </a>
+                    <p className="mt-1">{content.resume}</p>
+                    <div className="flex-start-row mt-2">
                         <div className="profile">
                             <img
-                                src=""
+                                src={user.imageProfile}
                                 className="profile-img"
                                 alt="Foto do perfil"
                             />
                         </div>
                         <div className="ml-1">
-                            <h6 className="color-primary">Jean Wilker</h6>
-                            <h6 className="color-gray">@wilker</h6>
+                            <h6 className="color-primary">
+                                {user.name} {user.surname}
+                            </h6>
+                            <h6 className="color-gray">{user.user}</h6>
                         </div>
                     </div>
                 </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,10 +5,33 @@ import Banner from './Banner';
 import Card from './Card';
 
 // FIGURES
-import imgStar from '../../svg/icon-star.svg'
+import imgStar from '../../svg/icon-star.svg';
 
+// API
+import api from '../../services/api';
+
+//HOOKS
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+    const [main, setMain] = useState([]);
+    const [banner, setBanner] = useState([]);
+    const [mostSeen, setMostSeen] = useState([]);
+
+    useEffect(() => {
+        api.get('/posts?star=5&_limit=2&_order=desc').then((response) => {
+            setMain(response.data);
+        });
+
+        api.get('/posts?_sort=date_order=desc&_limit=1').then((response) => {
+            setBanner(response.data);
+        });
+
+        api.get('/posts?-sort=views_order=desc&_limit=3').then((response) => {
+            setMostSeen(response.data);
+        });
+    }, []);
+
     return (
         <>
             <Hero />
@@ -16,16 +39,26 @@ const Home = () => {
             <section className="container">
                 <div className="row">
                     <div className="grid-6">
-                        <img src={imgStar} className="icon-l" alt="Icone de estrela dos favoritos" />
-                        <h3 className="mt-2">Os melhores e mais bem votados posts do mês.</h3>
-                    
+                        <img
+                            src={imgStar}
+                            className="icon-l"
+                            alt="Icone de estrela dos favoritos"
+                        />
+                        <h3 className="mt-2">
+                            Os melhores e mais bem votados posts do mês.
+                        </h3>
+
                         <p className="mt-2">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare urna pharetra ut ac, pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare urna pharetra ut ac, pellentesque.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Ornare urna pharetra ut ac, pellentesque.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Ornare urna pharetra ut ac, pellentesque.
                         </p>
                     </div>
 
-                    <Main />
-                    
+                    {main.map((item) => {
+                        return <Main key={item.id} content={item} />;
+                    })}
                 </div>
             </section>
 
@@ -34,9 +67,7 @@ const Home = () => {
             <section className="container">
                 <h3 className="ml-2 mb-3">Mais vistos</h3>
                 <div className="row">
-
                     <Card />
-
                 </div>
             </section>
         </>
