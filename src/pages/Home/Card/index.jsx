e41cba7987efa46e7187d8c2e4df46d9a52a410c
Jean Wilker <jeanwilkerss@gmail.com>
2022-11-27 21:29:28 -0300

    Cria interação de de link dos componentes banner e card.

--- src/pages/Home/Card/index.jsx
+++ src/pages/Home/Card/index.jsx
@@ -1,25 +1,27 @@
+import { Link } from "react-router-dom";
+
 const Card = ({ content }) => {
     return (
         <>
             <div className="grid-4 card hidden p-0">
                 <div className="thumb hidden">
-                    <a href="" className="p-0">
+                    <Link to={"/post/" + content.id} className="p-0">
                         <img src={content.imageUrl} alt="" />
-                    </a>
+                    </Link>
                 </div>
                 <div className="p-2">
                     <h6 className="color-gray">{content.date}</h6>
                     <h6 className="uppercase color-primary">{content.category}</h6>
-                    <a href="" className="link-title">
+                    <Link to={"/post/" + content.id} className="link-title">
                         <h4 className="mt-1">{content.title}</h4>
-                    </a>
+                    </Link>
 
                     <p className="my-2">
                         {content.resume}
                     </p>
-                    <a href="" className="link p-0">
+                    <Link to={"/post/" + content.id} className="link p-0">
                         Ler mais
-                    </a>
+                    </Link>
                 </div>
             </div>
         </>
